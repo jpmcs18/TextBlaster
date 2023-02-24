02caef0e486be5d1dafbeb0a4ce77564c7f59bd7
John Paul Manlangit <johnpaul.manlangit@outlook.com>
2023-02-24 08:45:32 +0800

    Add project files.

---
+++ TextBlaster/Program.cs
@@ -0,0 +1,24 @@
+﻿using DBUtilitiesStandard;
+using Microsoft.Extensions.Configuration;
+using ObjectExtension;
+
+var builder = new ConfigurationBuilder();
+builder.SetBasePath(Directory.GetCurrentDirectory())
+       .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+IConfiguration config = builder.Build();
+
+Console.WriteLine("Start...");
+
+Main:
+{
+    Console.WriteLine("Fetching data...");
+    using var db = new DBTools(config["ConnectionString"]);
+    //db.ExecuteReaderListStringAsync<>
+
+
+    Thread.Sleep(1000 * config["MinuteInterval"].ToInt());
+}
+
+goto Main;
+
